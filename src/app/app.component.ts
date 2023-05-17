cd1daa5d2c64e447530b38e90bca218ae50335b9
surekhatulasi <surekhatulasi1026@gmail.com>
2023-05-17 16:25:57 +0530

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import{GetApiService}from './get-api.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Welcome To Api';
+  constructor(private api:GetApiService) 
+  {
+    
+  }
+ngOnInit()
+{
+this.api.apiCall().subscribe((data)=>{
+  console.warn("get api data",data);
+  
+  
+})
+}
+}
